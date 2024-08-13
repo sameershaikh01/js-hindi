30153dd7d246bcae4611584dc69de72d624b59ab
sameershaikh01 <141818832+sameershaikh01@users.noreply.github.com>
2024-08-13 08:48:00 +0000

    Operation

--- 01_basics/03_datatypeConver.js
+++ 01_basics/03_datatypeConver.js
@@ -1,17 +1,51 @@
-let score = "33";
-let score1 = "234v"
+// let score = "33";
+// let score1 = "234v"
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
-let numberInVal = Number(score);
-let numberInVal1 = Number(score1);
+// let numberInVal = Number(score);
+// let numberInVal1 = Number(score1);
 
 
-console.log(typeof(numberInVal));
-console.log(typeof(numberInVal1));
-console.log(numberInVal1);
+// console.log(typeof(numberInVal));
+// console.log(typeof(numberInVal1));
+// console.log(numberInVal1);
 
 
 // "33" => 33
 //"33b" => naN not a number but type is number
 // true => 1 or false => 0
+
+
+/* **************************************** Operation in javascript ***************************************************   */
+
+let val = 3;
+let negVal = -val;
+console.log(negVal);
+
+let str1 = "hello"
+let str2 = " sameer"
+
+let str3 = str1 + str2;
+console.log(str3);
+
+// complex string
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+
+
+console.log("1" + 2 + 3); //123
+
+console.log(1 + 2 + "3"); //33
+console.log(1 + "2" + 3);  // 123
+
+let x = 3;
+console.log(x);
+x++
+console.log(x)
+
+let y = 3;
+console.log(y);
+++y
+console.log(y);
+
