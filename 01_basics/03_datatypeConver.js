c4c3c2e0b9889ce38998b3485b7c2f67e62eb262
sameershaikh01 <141818832+sameershaikh01@users.noreply.github.com>
2024-08-13 08:26:51 +0000

    datatypeConver

---
+++ 01_basics/03_datatypeConver.js
@@ -0,0 +1,17 @@
+let score = "33";
+let score1 = "234v"
+
+console.log(typeof(score));
+
+let numberInVal = Number(score);
+let numberInVal1 = Number(score1);
+
+
+console.log(typeof(numberInVal));
+console.log(typeof(numberInVal1));
+console.log(numberInVal1);
+
+
+// "33" => 33
+//"33b" => naN not a number but type is number
+// true => 1 or false => 0
