e4da8278362ace2c03df82a1d0105bdfb0a3ab61
sameershaikh01 <141818832+sameershaikh01@users.noreply.github.com>
2024-08-13 10:18:27 +0000

    datatype-summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,40 @@
+// primitive data
+
+/*
+String
+Number
+Bigint
+Boolean
+Undefined
+Null
+Symbol
+Object
+*/
+
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+
+
+// Reference type or non primitive data
+
+// Array , Object and Functions
