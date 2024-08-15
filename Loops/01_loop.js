76e8046a7e59b7d0ff0025a7a01a7f889a725452
sameershaikh01 <141818832+sameershaikh01@users.noreply.github.com>
2024-08-15 06:22:32 +0000

    forloop

---
+++ Loops/01_loop.js
@@ -0,0 +1,19 @@
+// for loop
+
+// for (expression 1; expression 2; expression 3) {
+//     // code block to be executed
+//   }
+
+// let text = 0
+// for (let i = 0; i < 5; i++) {
+//     text += "The number is " + i;
+//     console.log(text)
+//   }
+
+// const array = [1,2,3,4,5,6]
+
+// for(let i = 0; i< 10; i++){
+//     const element = i;
+//     console.log(element)
+// }
+
