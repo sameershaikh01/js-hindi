226171f86944f6af6535b62a7eafd1230cc20cda
sameershaikh01 <141818832+sameershaikh01@users.noreply.github.com>
2024-08-15 09:22:54 +0000

    while n do

---
+++ Loops/whileAndDoWhile.js
@@ -0,0 +1,57 @@
+// while loop
+
+// syntax of while loop
+
+// while(){
+//     code 
+// }
+
+// let myArray = [1,2 ,3, 4,5 ]
+// let arr = 0;
+
+// while(arr < myArray.length){
+//     console.log(myArray[arr]);
+//     arr++
+// }
+
+
+
+// let score = 11;
+
+// do {
+//      console.log(score)
+// }while(score < 10);
+
+
+
+// map
+
+// let map = new map()
+
+// map.set("IN" , "India")
+
+// array obect
+
+let myArray = [
+    {
+        name:"sameer",
+        age :22
+    },
+    {
+        name:"sam",
+        age :21
+    },
+    {
+        name:"srm",
+        age :24
+    },
+    {
+        name:"tom",
+        age :22
+    }
+]
+
+// item define object  in array
+myArray.forEach((item) => {
+    console.log(item.name)
+})
