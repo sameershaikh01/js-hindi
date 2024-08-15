4d4d157b7f9e8de40838ea78498611dbc462d477
sameershaikh01 <141818832+sameershaikh01@users.noreply.github.com>
2024-08-15 06:45:09 +0000

    loopIn n Of

---
+++ Loops/01_loopInandOf.js
@@ -0,0 +1,21 @@
+// for loop
+
+// const person = {fname:"John", lname:"Doe", age:25};
+
+
+// for(let x in person){
+//     console.log(person[x])
+// }
+
+
+/*  +++++++++++++++++++++++++++++++++++++++++++++ OF +++++++++++++++++++++++++++++++++ */
+
+// The JavaScript for of statement loops through the values of an iterable object.
+
+// It lets you loop over iterable data structures such as Arrays, Strings, Maps, NodeLists, and more:
+
+// const name = "sameer"
+
+// for(let Name of name){
+//     console.log(Name)
+// }
