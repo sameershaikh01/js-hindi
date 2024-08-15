6c7e0944db25033589763915e48735bb490ed74b
sameershaikh01 <141818832+sameershaikh01@users.noreply.github.com>
2024-08-15 04:06:43 +0000

    exe

---
+++ javascriptExecution/executionOnlyTxt.js
@@ -0,0 +1,32 @@
+/* 
+ excution of js
+
+ {} code => Global excution context  Global browser  this values always windows and node is differnt
+
+
+ Global execution context
+ Function execution context
+ Eval execution context
+ js is single threaded
+
+
+ code convert two phase 
+ 1. code convert into Memory Creation phase or Creation phase
+
+
+ 2.Execution phase
+
+
+ let val1 = 10
+ let val2 = 10
+
+ function myFunction(num1 , num2){
+   let total = num1 + num2
+   return total
+ }
+let result1 = myFunction(val1, val2)
+let result2 = myFunction(2, 3)
+
+
+
+*/
