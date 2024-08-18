2051d2cb46bd812bc2537cb2772b0d54f1d17ec7
sameershaikh01 <141818832+sameershaikh01@users.noreply.github.com>
2024-08-18 06:06:00 +0000

    aaaa

---
+++ Asynchronous/asyn.js
@@ -0,0 +1,20 @@
+/*
+   javascript is
+
+    Synchronous
+
+    Single thread
+
+    Execution context
+
+    execute one line of code at once
+
+
+    Blocking code                                          vs              Non-Blocking code
+    
+    Block the code                                                         Do not block the code
+
+                                                                           
+    read the sync                                                         read the file asyn
+
+ */
